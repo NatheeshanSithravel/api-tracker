8036f4429a60ef9d91930e3017184931293ae6c4
Natheeshan Sithravel <natheeshan.20244025@iit.ac.lk>
2025-04-18 11:24:06 +0530

    Add files via upload

--- src/main/java/com/apisettracker/model/ApiSetDetail.java
+++ src/main/java/com/apisettracker/model/ApiSetDetail.java
@@ -41,7 +41,7 @@ public class ApiSetDetail {
     private LocalDateTime addedAt;
 
     @CreatedBy
-    @Column(name = "ADDED_BY", length = 100, nullable = false)
+    @Column(name = "ADDED_BY", length = 100 , nullable = false, updatable = false)
     private String addedBy;
 
     @LastModifiedDate
